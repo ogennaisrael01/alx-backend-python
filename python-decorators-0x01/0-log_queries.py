bfcfa081252ff691912b9d8e15b76cee26b02805
ogennaisrael01 <ogennaisrael98@gmail.com>
2025-05-21 15:47:59 +0100

    python decorators fetch data from user table

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -7,22 +7,22 @@ from datetime import datetime
 def log_queries(func):
     @functools.wraps(func)
     def wrapper(*args, **kwags):
-            func(*args, **kwags)
+        print (func(*args, **kwags))
 
     return wrapper
 
 
-
+@log_queries
 def fetch_all_users(query):
     try:
         conn = sqlite3.connect('users.db')
         cursor = conn.cursor()
         cursor.execute(query)
         results = cursor.fetchall()
-        conn.close()
+        
     except sqlite3.Error as e:
          print(e)
-    print(results)
-
+    conn.close()
+    return results
 #### fetch users while logging the query
-users = fetch_all_users(query="SELECT * FROM users")
+fetch_all_users(query="SELECT * FROM users")

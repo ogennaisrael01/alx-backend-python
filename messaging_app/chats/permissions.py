c880144733b807d24cb46068c17fd4360f191b87
ogennaisrael01 <ogennaisrael98@gmail.com>
2025-06-08 10:34:55 +0100

    authentication and permissiion

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,9 +1,11 @@
 from rest_framework import permissions
-class IsOwnerOrReadOnly(permissions.BasePermission):
+class IsParticipantOfConversation(permissions.BasePermission):
     """
     Custom permission to only allow owners of an object to edit it.
     """
-
+    def has_permission(self, request, view):
+        # Check if the user is authenticated
+        return  request.user.is_authenticated
     def has_object_permission(self, request, view, obj):
         # Write permissions are only allowed to the owner of the object.
         return obj.owner == request.user
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,17 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Conversation, Messages
 from .serializers import ConversationSerializer, MessageSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from .permissions import IsOwnerOrReadOnly
+from .permissions import IsParticipantOfConversation
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ["title", "created_at"]
-    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]  # Ensure only authenticated users can access this view
+    permission_classes = [IsAuthenticated, IsParticipantOfConversation]  # Custom permission to check if user is a participant of the conversation
 
 
 
@@ -20,4 +20,19 @@ class MessageViewSet(viewsets.ModelViewSet):
     serializer_class = MessageSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ["sent_by", "sent_at", "conversation"]
-    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly] # Ensure only authenticated users can access this view
+    permission_classes = [IsAuthenticated, IsParticipantOfConversation]  # Custom permission to check if user is a participant of the conversation
+
+    def get_queryset(self):
+        queryset  = Messages.objects.all()
+        conversation_id = self.request.query_params.get('conversation_id', None)
+
+        if conversation_id is not None:
+            queryset = queryset.filter(conversation__conversation_id=conversation_id)
+            # Check if the user is a participant of the conversation
+                  
+            convesation  = get_object_or_404(Conversation, conversation_id=conversation_id)
+            if convesation.participants.filter(user_id=self.request.user.user_id).exists():
+                return queryset
+            else:
+                # If the user is not a participant, return an empty queryset
+                return Messages.objects.none()

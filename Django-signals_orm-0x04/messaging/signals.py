77beb6a0a404bbeff73859502730d5cd2aefcf4a
ogennaisrael01 <ogennaisrael98@gmail.com>
2025-06-12 12:07:18 +0100

    signals file

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,8 +1,8 @@
 from django.db.models.signals import post_save
-from django.dispatch import reciever
+from django.dispatch import receiver
 from messaging.models import Message, Notification
 
-@reciever(post_save, sender=Message, weak=False, dispatch_uid="Send a message")
+@receiver(post_save, sender=Message, weak=False, dispatch_uid="Send a message")
 def  message_signal(sender, instance, created, **kwargs):
     print("Signal fired....")
     if created:

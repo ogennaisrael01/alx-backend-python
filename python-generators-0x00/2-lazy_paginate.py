430179aeffade2bd5030c0e0ee5057fd7548b4a2
ogennnaisrael01 <ogennaisrael98@gmail.com>
2025-10-31 16:37:14 +0100

    feat: implemeted lazy pagination in filtering user data

--- python-generators-0x00/2-lazy_paginate.py
+++ python-generators-0x00/2-lazy_paginate.py
@@ -3,16 +3,17 @@ import seed
 def paginate_users(page_size, offset=0):
     connection = seed.connect_to_prodev()
     cursor = connection.cursor(dictionary=True)
-    cursor.execute(f"SELECT * FROM users ORDER BY name LIMIT {page_size} OFFSET {offset}")
+    cursor.execute(f"SELECT * FROM users LIMIT {page_size} OFFSET {offset}")
     rows = cursor.fetchall()
     connection.close()
     return rows
 
 
-def lazy_paginate(page_size):
-    for row in paginate_users(page_size=page_size):
+def lazy_paginate(page_size, page=0):
+    offset = page * page_size
+    for row in paginate_users(page_size=page_size, offset=offset):
         yield row
 
 if __name__ == "__main__":
-    for row in lazy_paginate(10):
+    for row in lazy_paginate(100):
        print(row)

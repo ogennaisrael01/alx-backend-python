a433068ae88478c6c31441ae0e9035624598dcf4
ogennnaisrael01 <ogennaisrael98@gmail.com>
2025-10-31 17:35:23 +0100

    feat: implemeted for calcualting total average age of all users

---
+++ python-generators-0x00/4-stream_ages.py
@@ -0,0 +1,24 @@
+from seed import connect_to_prodev
+import sys
+
+def stream_user_ages():
+    connection = connect_to_prodev()
+    cursor = connection.cursor(dictionary=True)
+    query = "SELECT u.age FROM users u "
+    cursor.execute(query)
+    result = cursor.fetchall()
+    return result
+
+
+def ages():
+    user_ages = stream_user_ages()
+    for age in user_ages:
+        yield age
+
+if __name__ == "__main__":
+    save_ages_in_list = list()
+    for row in ages():
+        save = int(row.get("age"))
+        save_ages_in_list.append(save)
+    avg = sum(save_ages_in_list) / len(save_ages_in_list)
+    print(f"Average age of users: {round(avg, 2)}")

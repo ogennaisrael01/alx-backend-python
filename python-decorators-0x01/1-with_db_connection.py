4ff5f3e1d854b1183c261e7f23596cf80a5097b2
ogennnaisrael01 <ogennaisrael98@gmail.com>
2025-11-06 12:03:00 +0100

    feat: implemented feature for authomatic db connection usign decorators:

---
+++ python-decorators-0x01/1-with_db_connection.py
@@ -0,0 +1,28 @@
+from sqlite3 import Connection
+import sqlite3
+import functools
+import datetime
+
+
+def with_db_connection(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        try :
+            connection = sqlite3.connect("users.db")
+            if connection:
+                response = func(connection, *args, **kwargs)
+        except sqlite3.Error as e:
+            raise ValueError(f"Error: {e}")
+
+        return response
+    return wrapper
+
+@with_db_connection 
+def get_user_by_id(conn: Connection, user_id): 
+    cursor = conn.cursor() 
+    cursor.execute("SELECT * FROM users WHERE id = ?", (user_id,)) 
+    return cursor.fetchall() 
+
+if __name__ == "__main__":
+    user = get_user_by_id(user_id=1)
+    print(user)

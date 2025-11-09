44b1eeda82e249738a1794296eb4d03d4c0020d8
ogennnaisrael01 <ogennaisrael98@gmail.com>
2025-11-09 19:59:00 +0100

    feat: concurrent Qurey

---
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -0,0 +1,28 @@
+
+import aiosqlite
+import asyncio
+
+async def connect_db():
+    try:
+        db_connection = await aiosqlite.connect("users.db") 
+        print("Connected to the users db successfully")
+        return db_connection     
+    except aiosqlite.Error as e:
+        raise ValueError(e)
+
+async def create_table():
+    db = await connect_db()
+    cursor = await db.cursor()
+    await cursor.execute("SELECT * FROM users")
+    result = await cursor.fetchall()
+    print("Executed sql query for fetching user data")
+    print(result)
+
+
+
+async def main():
+    task = await asyncio.gather(create_table())
+    print(task)
+
+if __name__ == "__main__":   
+    asyncio.run(main())

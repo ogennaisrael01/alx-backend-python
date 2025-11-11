4c13706c93aa272774dbffdb4a9c2db76006a453
ogennnaisrael01 <ogennaisrael98@gmail.com>
2025-11-11 17:26:59 +0100

    removed pycache from tracking

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ResgisterSerializer
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User = get_user_model()
+
+class RegisterViewset(viewsets.ModelViewSet):
+    permission_classes = []
+    serializer_class = ResgisterSerializer
+    queryset = User.objects.all()
+
+    

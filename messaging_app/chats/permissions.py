269df344a451f6abd75306a381eb8c4d6b0b24ee
ogennnaisrael01 <ogennaisrael98@gmail.com>
2025-11-21 14:53:26 +0100

    feat: added permission check for message and conversation view to only allow access to user within a convesation to modify the view

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -3,18 +3,17 @@ from .models import Conversation, Messages
 
 
 class IsPaticipantsOfConversation(permissions.BasePermission):
-    """ A custom permission to check for user in a conversation or actually is the host of that conversation """
+    """ A custom permission to check for user in a conversation"""
 
     def has_permission(self, request, view):
         return request.user.is_authenticated
     
 
     def has_object_permission(self, request, view, obj): 
-        # if obj is convesation
-        if isinstance(obj, Conversation):
-            return request.user in obj.participants.all()   
-        # if the obj is from the message class
-        elif isinstance(obj, Messages):
-            return request.user in obj.conversation.participants.all()
+        if not request.user.is_authenticated:
+            return False
+        if isinstance(obj, Messages):
+            if request.user in obj.conversation.participants.all():
+                return request.method in ["PUT", "PATCH", "DELETE", "GET"]
         
         return False

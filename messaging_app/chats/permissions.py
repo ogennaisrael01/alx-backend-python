6a7df8c1f2492a35b070de66788a24d9ce7a89c2
ogennnaisrael01 <ogennaisrael98@gmail.com>
2025-11-21 14:36:16 +0100

    feat: added permission check for message and conversation view to only allow access to user within a convesation to modify the view

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,8 +1,8 @@
-from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 from .models import Conversation, Messages
 
 
-class IsPaticipantsOfConversation(BasePermission):
+class IsPaticipantsOfConversation(permissions.BasePermission):
     """ A custom permission to check for user in a conversation or actually is the host of that conversation """
 
     def has_permission(self, request, view):
@@ -10,11 +10,9 @@ class IsPaticipantsOfConversation(BasePermission):
     
 
     def has_object_permission(self, request, view, obj): 
-
         # if obj is convesation
         if isinstance(obj, Conversation):
-            return request.user == obj.user
-        
+            return request.user in obj.participants.all()   
         # if the obj is from the message class
         elif isinstance(obj, Messages):
             return request.user in obj.conversation.participants.all()
--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -25,7 +25,8 @@ AUTH_USER_MODEL = "chats.CustomUser"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.BasicAuthentication'
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         "rest_framework.permissions.IsAuthenticated"

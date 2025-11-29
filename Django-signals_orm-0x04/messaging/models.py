7fb9407f40dae244676d2b8c9b4cdd39e504f410
ogennnaisrael01 <ogennaisrael98@gmail.com>
2025-11-29 08:49:30 +0100

    feat: added notif models for  in app notification when a message instance is created

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -1,2 +1,27 @@
 from django.db import models
+import uuid
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
+class Notification(models.Model):
+    notification_id = models.UUIDField(
+        primary_key=True,
+        db_index=True,
+        default=uuid.uuid4,
+        null=False,
+        max_length=20
+    )
+
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notifications_sender")
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notification_reciever")
+
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = "notifications"
+        verbose_name = "conversation"
+        ordering = ["-timestamp"]
+
+    def __str__(self):
+        return f"Nootiication('{self.notification_id}, {self.sender.username}, {self.reciever.username}, {self.is_read}, {self.timestamp})"
